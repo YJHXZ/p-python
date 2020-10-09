1e672f045291e3b97392ada42c9db89685ffbc06
acmi <46014904+YJHXZ@users.noreply.github.com>
2020-10-09 09:54:00 +0800

    Update tow.py

--- tow.py
+++ tow.py
@@ -29,3 +29,43 @@ def export_excel(hero):
 
 if __name__ == '__main__':
     export_excel('hero')
+    
+#文件较多的方式导出法
+# coding=utf-8
+import pymysql
+import openpyxl
+
+
+def export_excel(hero):
+    # 连接数据库，查询数据
+    host, user, passwd, db = '127.0.0.1', 'root', '12345678', 'RUNOOB'
+    conn = pymysql.connect(user=user, host=host, port=3306, passwd=passwd, db=db, charset='utf8')
+    cur = conn.cursor()
+    sql = 'select * from %s' % hero
+    cur.execute(sql)  # 返回受影响的行数
+
+    fields = [field[0] for field in cur.description]  # 获取所有字段名
+    all_data = cur.fetchall()  # 所有数据
+
+    # 写入excel
+    book = openpyxl.Workbook()
+    sheet = book.create_sheet(index=1)
+    ws = book.active
+
+    # for col, field in enumerate(fields):
+    #     sheet.write(0, col, field)
+
+    i = 1
+    r = 1
+    for line in all_data:
+        for col in range(1, len(line) + 1):
+            ColNum = r
+            ws.cell(row=r, column=col).value = line[col - 1]
+        i += 1
+        r += 1
+    # 工作簿保存到磁盘
+    book.save('%s.xlsx' % hero)
+
+
+if __name__ == '__main__':
+    export_excel('hero')
